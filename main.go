550ecae8f4d1f307ea90dfd0a55d5f81cbc56da5
Christopher Pisano <christopher.pisano@capitalone.com>
2019-06-27 11:39:04 -0400

    fix(argument): Fix Version Argument
    This commit fixes checking if an argument named `version` is present and
    taking the appropriate action.

--- main.go
+++ main.go
@@ -18,34 +18,59 @@ func main() {
 	helmVersion := "v2.10.0"
 	helmPath := "/usr/local/bin/helm"
 	helmRegex := `^darwin-amd64\/helm$`
-	version := "v0.1.0"
-	showVer := os.Args[1]
+	appVer := "v0.1.0"
 
-	if showVer != "" && showVer == "version" {
-		fmt.Println(version)
-	} else if showVer != "" && showVer != "version" {
-		log.Fatal("Unknown argument!")
-	} else {
-		if !(checks.Env("HTTP_PROXY") && checks.Env("http_proxy")) {
-			log.Fatal("Remember to set your proxy")
+	if len(os.Args) > 1 {
+		showVer := os.Args[1]
+		switch showVer {
+		case "version":
+			fmt.Println(appVer)
+			return
+		default:
+			log.Fatal("Unknown argument!")
 		}
+	}
 
-		if !checks.App("brew") {
-			resp := brew.Prompt()
-			switch resp {
-			case "yes":
-				if _, err := brew.Install(); err != nil {
-					log.Fatal(err)
-				}
-				log.Println("Homebrew has been installed")
-			case "no":
-				log.Fatal("We cannot proceed without Homebrew. You are on your own.")
-			default:
-				log.Fatal("Unknown response. Response must be 'yes' or 'no'.")
+	if !(checks.Env("HTTP_PROXY") && checks.Env("http_proxy")) {
+		log.Fatal("Remember to set your proxy")
+	}
+
+	if !checks.App("brew") {
+		resp := brew.Prompt()
+		switch resp {
+		case "yes":
+			if _, err := brew.Install(); err != nil {
+				log.Fatal(err)
 			}
+			log.Println("Homebrew has been installed")
+		case "no":
+			log.Fatal("We cannot proceed without Homebrew. You are on your own.")
+		default:
+			log.Fatal("Unknown response. Response must be 'yes' or 'no'.")
 		}
+	}
 
-		if !(checks.App("kubectl")) {
+	if !(checks.App("kubectl")) {
+		log.Println("Installing kubectl", kubectlVersion)
+		installer := kubectl.NewInstaller()
+		installer.DownloadURL = fmt.Sprintf(
+			"https://storage.googleapis.com/kubernetes-release/release/%s/bin/darwin/amd4/kubectl",
+			kubectlVersion,
+		)
+		installer.DownloadPath = kubectlPath
+
+		if _, err := installer.Install(); err != nil {
+			log.Fatal(err)
+		}
+		log.Println("kubectl", kubectlVersion, "Installed")
+	} else {
+		currVersion, err := kubectl.Version()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if checks.Version(currVersion, kubectlVersion) {
+			log.Println("kubectl version", currVersion, "is supported")
+		} else {
 			log.Println("Installing kubectl", kubectlVersion)
 			installer := kubectl.NewInstaller()
 			installer.DownloadURL = fmt.Sprintf(
@@ -57,36 +82,36 @@ func main() {
 			if _, err := installer.Install(); err != nil {
 				log.Fatal(err)
 			}
-			log.Println("kubectl", kubectlVersion, "Installed")
-		} else {
-			currVersion, err := kubectl.Version()
-			if err != nil {
-				log.Fatal(err)
-			}
 			if checks.Version(currVersion, kubectlVersion) {
-				log.Println("kubectl version", currVersion, "is supported")
+				log.Println("kubectl is now at a supported version:", kubectlVersion)
 			} else {
-				log.Println("Installing kubectl", kubectlVersion)
-				installer := kubectl.NewInstaller()
-				installer.DownloadURL = fmt.Sprintf(
-					"https://storage.googleapis.com/kubernetes-release/release/%s/bin/darwin/amd4/kubectl",
-					kubectlVersion,
-				)
-				installer.DownloadPath = kubectlPath
-
-				if _, err := installer.Install(); err != nil {
-					log.Fatal(err)
-				}
-				if checks.Version(currVersion, kubectlVersion) {
-					log.Println("kubectl is now at a supported version:", kubectlVersion)
-				} else {
-					log.Println("Failed to bring kubectl to a supported version")
-				}
+				log.Println("Failed to bring kubectl to a supported version")
 			}
 		}
+	}
 
-		if !(checks.App("helm")) {
-			log.Println("Installing helm", helmVersion)
+	if !(checks.App("helm")) {
+		log.Println("Installing helm", helmVersion)
+		installer := helm.NewInstaller()
+		installer.DownloadURL = fmt.Sprintf(
+			"https://get.helm.sh/helm-%s-darwin-amd64.tar.gz",
+			helmVersion,
+		)
+		installer.DownloadPath = helmPath
+		installer.HelmRegex = helmRegex
+
+		if _, err := installer.Install(); err != nil {
+			log.Fatal(err)
+		}
+		log.Println("helm", helmVersion, "installed")
+	} else {
+		currVersion, err := helm.Version()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if checks.Version(currVersion, helmVersion) {
+			log.Println("helm version", currVersion, "is supported")
+		} else {
 			installer := helm.NewInstaller()
 			installer.DownloadURL = fmt.Sprintf(
 				"https://get.helm.sh/helm-%s-darwin-amd64.tar.gz",
@@ -98,50 +123,30 @@ func main() {
 			if _, err := installer.Install(); err != nil {
 				log.Fatal(err)
 			}
-			log.Println("helm", helmVersion, "installed")
-		} else {
-			currVersion, err := helm.Version()
-			if err != nil {
-				log.Fatal(err)
-			}
 			if checks.Version(currVersion, helmVersion) {
-				log.Println("helm version", currVersion, "is supported")
+				log.Println("helm is now at a supported version:", helmVersion)
 			} else {
-				installer := helm.NewInstaller()
-				installer.DownloadURL = fmt.Sprintf(
-					"https://get.helm.sh/helm-%s-darwin-amd64.tar.gz",
-					helmVersion,
-				)
-				installer.DownloadPath = helmPath
-				installer.HelmRegex = helmRegex
-
-				if _, err := installer.Install(); err != nil {
-					log.Fatal(err)
-				}
-				if checks.Version(currVersion, helmVersion) {
-					log.Println("helm is now at a supported version:", helmVersion)
-				} else {
-					log.Println("Failed to bring helm to a supported version")
-				}
+				log.Println("Failed to bring helm to a supported version")
 			}
-		}
-
-		if !checks.App("kubectx") {
-			log.Println("Installing kubectx")
-			_, err := kubectx.Install()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println("kubectx installed")
-		}
-
-		if !checks.App("fzf") {
-			log.Println("Installing fzf")
-			_, err := kubectx.Fzf()
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println("fzf installed")
 		}
 	}
+
+	if !checks.App("kubectx") {
+		log.Println("Installing kubectx")
+		_, err := kubectx.Install()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("kubectx installed")
+	}
+
+	if !checks.App("fzf") {
+		log.Println("Installing fzf")
+		_, err := kubectx.Fzf()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("fzf installed")
+	}
+
 }
